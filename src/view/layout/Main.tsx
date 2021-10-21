ac75a49ad4885f618b94d00fc229cf95f9bda868
何东升 <he77505357@163.com>
2021-10-21 17:41:31 +0800

    基本布局

--- src/view/layout/Main.tsx
+++ src/view/layout/Main.tsx
@@ -1,34 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import {Link} from 'react-router-dom';
+import {Button} from 'antd';
 import useRequest from '../../hooks/useRequest';
 import Api from '../../axiosRequest/Api';
+
+import './main.less'
 
 const Main: React.FunctionComponent = () => {
     //  this is main layout page
     console.log('main');
-    const { run: getCats, response } = useRequest(Api.getCats);
-    const { run: createCats } = useRequest(Api.createCats, { manual: true });
+    const {run: getCats, response} = useRequest(Api.getCats);
+    const {run: createCats} = useRequest(Api.createCats, {manual: true});
 
     function requestTest() {
         getCats();
     }
 
     return (
-        <div>
-            <h3>this is main page</h3>
-            <div>
-                <Link to="/login"> to login Page </Link>
-                <Link to="/registry"> to registry Page </Link>
+        <div className='appMainPage'>
+            <div  className='mainMenu'>
+                menu
             </div>
-            <div>this is page content</div>
-            <div>
-                <Button onClick={requestTest}>request</Button>
-                <Button onClick={createCats}>need Login</Button>
-            </div>
-            <div>
-                <p>this is response </p>
-                <p>{response}</p>
+            <div className='mainContentWrap'>
+                <div className='mainHeader'>
+                    header
+                </div>
+                <div className='mainContent'>
+                    <div style={{height: '1000px'}}>
+                        this is page content
+                        <h3>this is main page</h3>
+                        <div>
+                            <Link to="/login"> to login Page </Link>
+                            <Link to="/registry"> to registry Page </Link>
+                        </div>
+                        <div>
+                            <p>this is response </p>
+                            <p>{response}</p>
+                        </div>
+                        <div>
+                            <Button onClick={requestTest}>request</Button>
+                            <Button onClick={createCats}>need Login</Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
